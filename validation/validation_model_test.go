b70d0e0c910232b86fb8b42d09a3b4c2391189c0
Traci Kamp <traci.kamp@gmail.com>
2023-12-31 11:32:40 -0700

    chore(blocks): add FromMap func to validationModel
    Adds a FromMap function to the validationModel type, which populates the struct from the values
    supplied in the map.
    
    See #1

--- validation/validation_model_test.go
+++ validation/validation_model_test.go
@@ -33,3 +33,79 @@ func TestValidationDocument_Diag(t *testing.T) {
 
 	assert.Equal(t, diag.Warning, d.Severity)
 }
+
+func TestFromMap(t *testing.T) {
+
+	t.Run("Basic", func(t *testing.T) {
+		m := make(map[string]interface{})
+
+		m[conditionKey] = true
+		m[summaryKey] = "test summary"
+		m[detailsKey] = "some details for you here"
+
+		v := &validationDocument{}
+
+		assert.NoError(t, v.FromMap(m))
+		assert.NotEmpty(t, v.details)
+		assert.Empty(t, v.severity)
+	})
+
+	t.Run("NoCondition", func(t *testing.T) {
+		m := make(map[string]interface{})
+
+		m[summaryKey] = "test summary"
+
+		v := &validationDocument{}
+
+		assert.Error(t, v.FromMap(m))
+
+		assert.Empty(t, v.condition)
+		assert.Empty(t, v.summary)
+		assert.Empty(t, v.details)
+		assert.Empty(t, v.severity)
+	})
+
+	t.Run("ConditionWrongType", func(t *testing.T) {
+		m := make(map[string]interface{})
+
+		m[conditionKey] = "test summary"
+
+		v := &validationDocument{}
+
+		assert.Error(t, v.FromMap(m))
+
+		assert.Empty(t, v.condition)
+		assert.Empty(t, v.summary)
+		assert.Empty(t, v.details)
+		assert.Empty(t, v.severity)
+	})
+
+	t.Run("NoSummary", func(t *testing.T) {
+		m := make(map[string]interface{})
+
+		m[conditionKey] = true
+
+		v := &validationDocument{}
+
+		assert.Error(t, v.FromMap(m))
+		assert.Empty(t, v.condition)
+		assert.Empty(t, v.details)
+		assert.Empty(t, v.severity)
+	})
+
+	t.Run("SummaryWrongType", func(t *testing.T) {
+		m := make(map[string]interface{})
+
+		m[conditionKey] = true
+		m[summaryKey] = 1234
+
+		v := &validationDocument{}
+
+		assert.Error(t, v.FromMap(m))
+		assert.Empty(t, v.condition)
+		assert.Empty(t, v.details)
+		assert.Empty(t, v.severity)
+
+	})
+
+}
